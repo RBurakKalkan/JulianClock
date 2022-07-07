410837f714c3291470a5d21ae4946beb2a6282a8
Burak Kalkan <burak.kalkan26@gmail.com>
2022-07-07 18:17:44 +0300

    Add project files.

---
+++ JulianClock/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace JulianClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_tarih = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_Utc = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(175, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(72, 102);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(1990, 1, 1, 0, 0, 0, 0);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 302);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(195, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Julian Day";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Normal Tarih";
+            // 
+            // lbl_tarih
+            // 
+            this.lbl_tarih.AutoSize = true;
+            this.lbl_tarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_tarih.Location = new System.Drawing.Point(175, 70);
+            this.lbl_tarih.Name = "lbl_tarih";
+            this.lbl_tarih.Size = new System.Drawing.Size(41, 13);
+            this.lbl_tarih.TabIndex = 0;
+            this.lbl_tarih.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "UTC";
+            // 
+            // lbl_Utc
+            // 
+            this.lbl_Utc.AutoSize = true;
+            this.lbl_Utc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Utc.Location = new System.Drawing.Point(175, 54);
+            this.lbl_Utc.Name = "lbl_Utc";
+            this.lbl_Utc.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Utc.TabIndex = 0;
+            this.lbl_Utc.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "1 den itibaren taranan açı (max 360)";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 531);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lbl_Utc);
+            this.Controls.Add(this.lbl_tarih);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Julian Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_tarih;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_Utc;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ JulianClock/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JulianClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DateTime jd, begin, today;
+        TimeSpan difference;
+        double a, b, c, ss;
+        int WIDTH = 300, HEIGHT = 300, secHAND = 140;
+        int cx, cy;
+        Bitmap bmp;
+        Graphics g;
+        Int32 Days;
+        DateTime simdi = DateTime.Now;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime simdi = DateTime.Now;
+            simdi = simdi.ToUniversalTime();
+            today = DateTime.UtcNow;
+            today = today.AddHours(-12);
+            begin = new DateTime(2016, 5, 10, today.Hour, today.Minute, today.Second);
+            begin = begin.AddHours(-12);
+            difference = today - begin;
+            Days = 2457519 + difference.Days;
+            jd = simdi.AddHours(-12);
+            a = jd.Hour;
+            b = jd.Minute;
+            c = jd.Second;
+            ss = (b / 60 + c / 3600 + a) / 24 ;
+            label1.Text = (Days + (b / 60 + c / 3600 + a) / 24).ToString("0.0000000");
+            g = Graphics.FromImage(bmp);
+            int[] handCoord = new int[2];
+            g.Clear(Color.White);
+            g.DrawEllipse(new Pen(Color.Black, 1f), 0, 0, WIDTH, HEIGHT);
+            g.DrawString("1", new Font("Arial", 12), Brushes.Black, new PointF(140, 0));
+            g.DrawString("0.25", new Font("Arial", 12), Brushes.Black, new PointF(266, 140));
+            g.DrawString("0.50", new Font("Arial", 12), Brushes.Black, new PointF(142, 282));
+            g.DrawString("0.75", new Font("Arial", 12), Brushes.Black, new PointF(0, 140));
+            handCoord = msCoord(ss, secHAND);
+            g.DrawLine(new Pen(Color.Red, .5F), new Point(150, cy), new Point(handCoord[0], handCoord[1]));
+            pictureBox1.Image = bmp;
+            g.Dispose();
+            lbl_tarih.Text = DateTime.Now.ToString();
+            lbl_Utc.Text = DateTime.Now.ToUniversalTime().ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime born;
+            TimeSpan fark;
+            born = dateTimePicker1.Value;
+            fark = DateTime.Now - born;
+            MessageBox.Show(born.ToShortDateString()+" ve "+ DateTime.Now.ToShortDateString() + " arasında "+ fark.Days.ToString()+" gün var.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(WIDTH + 1, HEIGHT + 1);
+            cx = WIDTH / 2;
+            cy = HEIGHT / 2;
+            pictureBox1.BackColor = Color.White;
+        }
+        private int[] msCoord(double val, int hlem)
+        {
+            double x;
+            int[] coord = new int[2];
+            x = 360 * val;
+            label2.Text = x.ToString("0.000000");
+            if (val >= 0 && val <= 360)
+            {
+                coord[0] = cx + (int)(hlem * Math.Sin(Math.PI * x / 180));
+                coord[1] = cx - (int)(hlem * Math.Cos(Math.PI * x / 180));
+            }
+            else 
+            {
+                coord[0] = cx + (int)(hlem * -Math.Sin(Math.PI * x / 180));
+                coord[1] = cx - (int)(hlem * Math.Cos(Math.PI * x / 180));
+            }
+            return coord;
+        }
+    }
+}
